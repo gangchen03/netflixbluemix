645564f684da0dab585f42afac4529dc26a70079
Gang Chen <gangchen@us.ibm.com>
2016-03-16 16:37:52 -0500

    integrate App bulild in docker-compose

---
+++ buildAll.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+##############################################################################
+##
+##  Wrapper sript to build all subproject
+##
+##############################################################################
+
+echo 'Build all projects...'
+
+echo 'Build Eureka Server ***'
+cd eurekaserver
+./gradlew build
+cp build/libs/eureka-spring-boot-0.1.0.jar docker/app.jar
+cd ..
+
+
+echo 'Build Zuul Proxy ***'
+cd zuulproxy
+./gradlew build
+cp build/libs/zuul-spring-boot-0.1.0.jar docker/app.jar
+cd ..
+
+echo 'Build MySQL microservice ***'
+cd microservice
+./gradlew build
+cp build/libs/gs-spring-boot-0.1.0.jar docker/app.jar
+cd ..
+
+
+echo 'Build DB2 microservice ***'
+cd db2microservice
+./gradlew build
+cp build/libs/gc-spring-boot-db2-0.1.0.jar docker/app.jar
+cd ..
+
+echo 'Build MQ microservice ***'
+cd mqmicroservice
+./gradlew build
+cp build/libs/gc-rabbitmq-0.1.0.jar docker/app.jar
+cd ..
+
+echo 'Completed All Builds successfully!'
