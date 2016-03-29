d7452e88ce5da1277e67de96da77d0ccb75aaca9
Rick Osowski <rosowski@gmail.com>
2016-03-29 18:38:14 -0500

    Added defaultBranch parameter to clonePeers
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- clonePeers.sh
+++ clonePeers.sh
@@ -21,30 +21,32 @@ if [ ${?} -ne 0 ]; then
   exit 1
 fi
 
+DEFAULT_BRANCH=${1:-development}
+
 echo -e '\nClone netflixbluemix-core project'
 REPO=${REPO_CORE}
 PROJECT=$(echo ${REPO} | cut -d/ -f5)
-git clone ${REPO} ../${PROJECT}
+git clone -b ${DEFAULT_BRANCH} ${REPO} ../${PROJECT}
 
 echo -e '\nClone netflixbluemix-db2 project'
 REPO=${REPO_DB2}
 PROJECT=$(echo ${REPO} | cut -d/ -f5)
-git clone ${REPO} ../${PROJECT}
+git clone -b ${DEFAULT_BRANCH} ${REPO} ../${PROJECT}
 
 echo -e '\nClone netflixbluemix-mq project'
 REPO=${REPO_MQ}
 PROJECT=$(echo ${REPO} | cut -d/ -f5)
-git clone ${REPO} ../${PROJECT}
+git clone -b ${DEFAULT_BRANCH} ${REPO} ../${PROJECT}
 
 echo -e '\nClone netflixbluemix-mysql project'
 REPO=${REPO_MYSQL}
 PROJECT=$(echo ${REPO} | cut -d/ -f5)
-git clone ${REPO} ../${PROJECT}
+git clone -b ${DEFAULT_BRANCH} ${REPO} ../${PROJECT}
 
 echo -e '\nClone netflixbluemix-elasticsearch project'
 REPO=${REPO_ES}
 PROJECT=$(echo ${REPO} | cut -d/ -f5)
-git clone ${REPO} ../${PROJECT}
+git clone -b ${DEFAULT_BRANCH} ${REPO} ../${PROJECT}
 
 echo -e '\nCloned all peer projects successfully!\n'
 ls ../ | grep netflixbluemix
