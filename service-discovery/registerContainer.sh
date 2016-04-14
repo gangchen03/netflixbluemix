5c02240765f3ab881a112419408cae486290e45c
Rick Osowski <rosowski@gmail.com>
2016-04-14 14:42:00 -0500

    Corrected typo in master IP variable

--- service-discovery/registerContainer.sh
+++ service-discovery/registerContainer.sh
@@ -9,7 +9,7 @@
 
 AUTHTOKEN=${1}
 SERVICENAME=${2}
-DEFAULT_TTL=1800
+DEFAULT_TTL=600
 SDENDPOINT="https://servicediscovery.ng.bluemix.net/api/v1"
 CURL='/usr/bin/curl'
 THISHOST=$(hostname)
@@ -34,27 +34,25 @@ then
     #echo $SERVICEINFO
 
     # if not, register itself as master/first node
-    if [ ${SERVICEINFO} == "null" ]; then
+    if [ "${SERVICEINFO}" == "null" ]; then
       echo "Service does not exist, will register as first node..."
       NEWSERVICE="{\"service_name\":\"${SERVICENAME}\", \"endpoint\": {\"type\":\"tcp\", \"value\":\"{${THISIP}}\"}, \"status\":\"UP\", \"tags\": [\"${THISHOST}\"], \"ttl\":${DEFAULT_TTL}, \"metadata\":{\"node\":1}}"
       echo ${NEWSERVICE}
       NEWSERVICEINFO=$(curl -s -X POST -H "Authorization: Bearer ${AUTHTOKEN}" -H "Content-Type: application/json" "${SDENDPOINT}/instances" -d "${NEWSERVICE}")
       echo ${NEWSERVICEINFO}
-      echo "Executing command to create cluster master instance:"
-      echo ${MASTER_COMMAND}
+      echo "Executing command to create cluster master instance:" ${MASTER_COMMAND}
       ${MASTER_COMMAND}
     else
       echo "Pull existing node data and register as a peer... "
       SERVICEINFO=$(curl -s -X GET -H "Authorization: Bearer ${AUTHTOKEN}" "${SDENDPOINT}/services/${SERVICENAME}" | jq '.')
-      MASTERIP=$(echo ${SERVICEINFO} | jq '.instances[0].endpoint.value' | sed 's/{//g;s/}//g;s/\"//g;')
+      MASTER_IP=$(echo ${SERVICEINFO} | jq '.instances[0].endpoint.value' | sed 's/{//g;s/}//g;s/\"//g;')
 
       #TODO NEED TO POST & REGISTER PEER INSTANCE HERE
 
       echo "Cluster Master IP Address:" ${MASTER_IP}
       JOIN_IP=${MASTER_IP}
 
-      echo "Executing command to create cluster peer instance:"
-      echo ${PEER_COMMAND}
+      echo "Executing command to create cluster peer instance:" ${PEER_COMMAND}
       eval ${PEER_COMMAND}
 
     fi
