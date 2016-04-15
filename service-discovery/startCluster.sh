a3842d5b4dc9799c52f07d727a4a1a84ca65e9db
Gang Chen <gangchen@us.ibm.com>
2016-04-15 15:52:53 -0500

    Fix the startCluster script for Consul, validate cluster creation successfully

--- service-discovery/startCluster.sh
+++ service-discovery/startCluster.sh
@@ -7,13 +7,14 @@
 ##
 ##############################################################################
 COUNTER=''
+SERVICE_DISCOVERY_TOKEN="rl3j00e06ee9e85emtrt3o012cp3ahpm8hqg2qsvku7olpafs78"
 
 function help () {
     echo ""
     echo "Simple script to start a cluster of Bluemix containers"
     echo ""
     echo "    startCluster.sh serviceName numberOfClusterMember";
-    echo "    serviceNam             -    Conul, RabbitMQ, Eureka, ElasticSearch etc.";
+    echo "    serviceName             -    Conul, RabbitMQ, Eureka, ElasticSearch etc.";
     echo "    numberOfClusterMember  -    number of cluster members to be created and started";
 }
 
@@ -26,12 +27,6 @@ then
           # create the first container
           cf ic run --name consul1 -p 53/tcp -p 53/udp -p 8300/tcp -p 8301/tcp -p 8301/udp -p 8302/tcp -p 8302/udp -p 8400/tcp -p 8500/tcp --memory 256  registry.ng.bluemix.net/chrisking/consul
 
-          #after the instance created, it needs to be restarted to successfully register to SD
-          /bin/sleep 60
-          cf ic stop consul1
-          /bin/sleep 30
-          cf ic start consul1
-
           #loop to check the first container has register in Bluemix Service Discovery
           while true
           do
@@ -39,6 +34,7 @@ then
             COUNTER=$(curl -X GET -H "Authorization: bearer ${SERVICE_DISCOVERY_TOKEN}" "https://servicediscovery.ng.bluemix.net/api/v1/services/consul" | cut -b 3-7)
             echo $COUNTER
             if [ $COUNTER == "Error" ]; then
+                  echo "Master node not found, wait for 10 second to retry"
                   /bin/sleep 10
             else
               echo "First node has successfuly registered with Service Discovery"
@@ -49,10 +45,6 @@ then
           echo "Create the second cluster member"
           #Create the second container
           cf ic run --name consul2 -p 53/tcp -p 53/udp -p 8300/tcp -p 8301/tcp -p 8301/udp -p 8302/tcp -p 8302/udp -p 8400/tcp -p 8500/tcp --memory 256  registry.ng.bluemix.net/chrisking/consul
-          /bin/sleep 60
-          cf ic stop consul2
-          /bin/sleep 30
-          cf ic start consl2
 
 
           ;;
